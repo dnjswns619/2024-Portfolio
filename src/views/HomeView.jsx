51f0700b13b49b3e810c4c52f4f909b8ad25f8d5
dnjswns619 <dnjswns619@naver.com>
2024-05-28 22:23:39 +0900

    Init Project

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/Header";
+import Main from "../components/main";
+import Footer from "../components/Footer";
+
+const HomeView = () => {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default HomeView;
