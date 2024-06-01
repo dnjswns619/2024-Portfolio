d98e1af9f3250f6b835319cc2b9e3d9fa7abee30
dnjswns619 <dnjswns619@naver.com>
2024-06-01 22:08:57 +0900

    feat : Add header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,24 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <h1 className="header__logo">
+        <a href="#intro">Jun's Web Portfolio</a>
+      </h1>
+      <nav className="header__nav nav">
+        <ul className="nav__wrap">
+          <li className="nav__item">
+            <a href="#about">About</a>
+          </li>
+          <li className="nav__item">
+            <a href="#port">Portfolio</a>
+          </li>
+          <li className="nav__item">
+            <a href="#work">Work</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
