992f0e4224e59a1ebe6e46078a486006f5823e8b
dnjswns619 <dnjswns619@naver.com>
2024-06-09 19:41:19 +0900

    feat : Add portfolio section animation, scss code

--- src/constants/index.js
+++ src/constants/index.js
@@ -78,3 +78,48 @@ export const introText = {
     }
   ]
 }
+
+export const portContent = [
+  {
+    title: "MCST",
+    cardText : "MCST",
+    skil: ["html", "css", "javascript"],
+    desc: `문화체육관광부의 메인 페이지를 접근성을 고려하며 반응형으로 클론 코딩을 진행하였습니다.
+    모든 컨텐츠를 tab키로 이동할수 있도록 했으며 a태그의 title, img태그의 alt 작성으로
+    눈으로 보지 못하는 사용자도 컨텐츠를 이해할 수 있도록 제작했습니다`,
+    gitLink: `https://github.com/dnjswns619/MCST`,
+    imgSrc: "intro_circle0"
+  },
+  {
+    title: "Apple",
+    skil: ["html", "css", "javascript"],
+    desc: `Apple 홈페이지의 AirPods 3세대 소개 홈페이지를 만들었습니다<br>
+    섹션 1, 2, 3은 스크롤에 따라 canvas에 이미지를 연속으로 그려
+    고해상도의 이미지가 깨지지 않고 부드럽게 스크롤 하며 텍스트들이
+    스크롤 정도에 따라 나오거나 들어가도록 제작하였습니다.<br>
+    섹션 4 또한 스크롤에 따라 canvas에 이미지를 연속으로 그렸지만
+    일정구간을 스크롤할때 canvas의 scale을 조절해 뒤에 나오는
+    grid 컨텐츠들과 이어지도록 javascript를 이용해 제작하였습니다.<br>
+    섹션 5와 6은 grid와 flex를 이용해 각 컨텐츠들을 일정하게 배치 하였습니다.`,
+    gitLink: `https://github.com/dnjswns619/Apple_AirPods`,
+    imgSrc: "intro_circle0"
+  },
+  {
+    title: "Enterpriseblockchain",
+    skil: ["html", "scss", "javascript", "gsap"],
+    desc: `gsap 플러그인을 사용하여 사용사의 스크롤에 따라 변화하는 엔터프라이즈블록체인을 제작했습니다.
+    observer API를 같이 사용해 사용자가 특정 섹션에 들어왔을때 header 메뉴가 변화하도록 제작하였으며,
+    gsap의 scrollTrigger.timeline을 사용해 반응형에서도 컨텐츠들의 위치들이 무너지지 않고 서로 다른 애니메이션 효과등을 사용하도록 제작하였습니다.`,
+    gitLink: `https://github.com/dnjswns619/enterpriseblockchain`,
+    imgSrc: "intro_circle0"
+  },
+  {
+    title: "Kream",
+    skil: ["react", "scss", "javascript"],
+    desc: `react를 사용해 kream 메인 페이지를 제작했습니다. 
+    슬라이더는 swiper slide를 사용하였고 컴포넌트를 나누고 여러 데이터들을 전달하는 과정에서 react를 이해하며 사용할 수 있었습니다.
+    `,
+    gitLink: `https://github.com/dnjswns619/Kream`,
+    imgSrc: "intro_circle0"
+  }
+]
