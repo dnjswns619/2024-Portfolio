8b5db39e6736d77646be6d67f94e911d150aefc5
dnjswns619 <dnjswns619@naver.com>
2024-06-23 17:46:20 +0900

    style : Modify projectDesc props name

--- src/components/ProjectDesc.jsx
+++ src/components/ProjectDesc.jsx
@@ -1,20 +1,20 @@
-const ProjectDesc = ({port}) => {
+const ProjectDesc = ({data}) => {
   return (
     <div className="projectDesc">
-      <div className="projectDesc__title">{port.title}</div>
+      <div className="projectDesc__title">{data.title}</div>
         <div className="projectDesc__skill">
           <div className="projectDesc__skill--tit">Language :</div>
-          <div className="projectDesc__skill--text">{port.skill.join(", ")}</div>
+          <div className="projectDesc__skill--text">{data.skill.join(", ")}</div>
         </div>
         <div className="projectDesc__desc">
           <div className="projectDesc__desc--tit">Description :</div>
-          <p className="projectDesc__desc--text" dangerouslySetInnerHTML={{__html: port.desc}}></p>
+          <p className="projectDesc__desc--text" dangerouslySetInnerHTML={{__html: data.desc}}></p>
         </div>
         {
-          port.gitLink ? 
+          data.gitLink ? 
           <div className="projectDesc__git-link">
             <div className="projectDesc__git-link--tit">GitHub: </div>
-            <a href={port.gitLink} className="projectDesc__gitLink">{port.gitLink}</a>
+            <a href={data.gitLink} className="projectDesc__gitLink">{data.gitLink}</a>
           </div> : null
         }
     </div>
