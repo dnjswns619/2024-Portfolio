46396cb66c6a8c177bdc6e64b9b41e493a156ea4
dnjswns619 <dnjswns619@naver.com>
2024-06-30 23:30:16 +0900

    feat : Add portSection reponsive code about tablet

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,11 +10,11 @@ export const introText = {
     },
     {
       class: "desc__item--text",
-      text: "Scrupulous"
+      text: "Growing"
     },
     {
       class: "desc__item--text kor",
-      text: "꼼꼼한"
+      text: "성장하는"
     },
     {
       class: "desc__item--text",
@@ -54,19 +54,19 @@ export const introText = {
   bottom: [
     {
       class: "desc__item--text",
+      text: "Scrupulous"
+    },
+    {
+      class: "desc__item--text kor",
+      text: "꼼꼼한"
+    },
+    {
+      class: "desc__item--text",
       text: "Enthusiastic"
     },
     {
       class: "desc__item--text kor",
       text: "열정적인"
-    },
-    {
-      class: "desc__item--text",
-      text: "Growing"
-    },
-    {
-      class: "desc__item--text kor",
-      text: "성장하는"
     },
     {
       class: "desc__item--text",
@@ -84,7 +84,7 @@ export const portContent = [
     title: "MCST",
     cardText : "MCST",
     skill: ["html", "css", "javascript"],
-    desc: `문화체육관광부의 메인 페이지를 접근성을 고려하며 반응형으로 클론 코딩을 진행하였습니다.
+    desc: `문화체육관광부의 메인 페이지를 접근성을 고려하며 반응형으로 클론 코딩을 진행하였습니다.<br>
     모든 컨텐츠를 tab키로 이동할수 있도록 했으며 a태그의 title, img태그의 alt 작성으로
     눈으로 보지 못하는 사용자도 컨텐츠를 이해할 수 있도록 제작했습니다`,
     gitLink: `https://github.com/dnjswns619/MCST`,
@@ -93,30 +93,26 @@ export const portContent = [
   {
     title: "Apple",
     skill: ["html", "css", "javascript"],
-    desc: `Apple 홈페이지의 AirPods 3세대 소개 홈페이지를 만들었습니다<br>
-    섹션 1, 2, 3은 스크롤에 따라 canvas에 이미지를 연속으로 그려
+    desc: `Apple 홈페이지의 AirPods 3세대 소개 홈페이지를 만들었습니다.<br>
+    스크롤에 따라 canvas에 이미지를 연속으로 그려
     고해상도의 이미지가 깨지지 않고 부드럽게 스크롤 하며 텍스트들이
-    스크롤 정도에 따라 나오거나 들어가도록 제작하였습니다.<br>
-    섹션 4 또한 스크롤에 따라 canvas에 이미지를 연속으로 그렸지만
-    일정구간을 스크롤할때 canvas의 scale을 조절해 뒤에 나오는
-    grid 컨텐츠들과 이어지도록 javascript를 이용해 제작하였습니다.<br>
-    섹션 5와 6은 grid와 flex를 이용해 각 컨텐츠들을 일정하게 배치 하였습니다.`,
+    스크롤 정도에 따라 나오거나 들어가도록 제작하였습니다.`,
     gitLink: `https://github.com/dnjswns619/Apple_AirPods`,
     imgSrc: "intro_circle0"
   },
   {
     title: "Enterprise blockchain",
     skill: ["html", "scss", "javascript", "gsap"],
-    desc: `gsap 플러그인을 사용하여 사용사의 스크롤에 따라 변화하는 엔터프라이즈블록체인을 제작했습니다.
+    desc: `gsap 플러그인을 사용하여 사용사의 스크롤에 따라 변화하는 엔터프라이즈블록체인을 제작했습니다.<br>
     observer API를 같이 사용해 사용자가 특정 섹션에 들어왔을때 header 메뉴가 변화하도록 제작하였으며,
-    gsap의 scrollTrigger.timeline을 사용해 반응형에서도 컨텐츠들의 위치들이 무너지지 않고 서로 다른 애니메이션 효과등을 사용하도록 제작하였습니다.`,
+    반응형에서도 컨텐츠들의 위치들이 무너지지 않고 서로 다른 애니메이션 효과등을 사용하도록 제작하였습니다.`,
     gitLink: `https://github.com/dnjswns619/enterpriseblockchain`,
     imgSrc: "intro_circle0"
   },
   {
     title: "Kream",
     skill: ["react", "scss", "javascript"],
-    desc: `react를 사용해 kream 메인 페이지를 제작했습니다. 
+    desc: `react를 사용해 kream 메인 페이지를 제작했습니다.<br>
     슬라이더는 swiper slide를 사용하였고 컴포넌트를 나누고 여러 데이터들을 전달하는 과정에서 react를 이해하며 사용할 수 있었습니다.
     `,
     gitLink: `https://github.com/dnjswns619/Kream`,
@@ -206,21 +202,26 @@ export const workContent = [
   {
     title: "초등학교 3,4학년 과학",
     skill: ["html", "css", "javascript"],
-    desc: `천재교육에서 진행하는 초등학교 3,4 학년 전자저작물 제작에 참여했습니다. 입사후 처음으로 진행한 프로젝트이며 200페이지가 넘는 쪽수를 개발 했습니다. 디자인팀과 소통하고 개발이 필요할 때에는 개발 팀과 소통하며 업무를 진행해 나아갔고 html과 css를 이용해 작업하였으며 개발팀에서 제작해 놓았던 템플릿과 개발된 코드등을 수정해 다른 페이지에도 적용해가며 촉박한 기한에도 불구하고 완성시켜 제출할 수 있엇습니다.`,
+    desc: `천재교육에서 진행하는 초등학교 3,4 학년 전자저작물 제작에 참여했습니다.<br>
+    입사후 처음으로 진행한 프로젝트이며 디자인팀과 소통하고 개발이 필요할 때에는 개발 팀과 소통하며 업무를 진행해 나아갔고 200페이지가 넘는 쪽수를 개발 했습니다.<br>
+    html과 css를 이용해 작업하였으며 개발팀에서 제작해 놓았던 템플릿과 개발된 코드등을 수정해 다른 페이지에도 적용해가며 촉박한 기한에도 불구하고 완성시켜 제출할 수 있엇습니다.`,
     imgSrc1: "code1",
     imgSrc2: "code1"
   },
   {
     title: "AIDT 고등학교 영어",
     skill: ["html", "css"],
-    desc: `천재교육에서 진행하는 AIDT 고등학교 영어에 선임 퍼블리셔로 제작에 참여했습니다. 신입 혹은 다른 팀원 분들이 쉽게 제작할 수 있도록 하기위한 프로토 제작과 다른 여러 페이지 제작을 도맡아 했으며 디자인팀, 개발팀, 그리고 기획사와도 소통하며 제작을 하였습니다. 프로젝트에서 처음 시도하는 api를 연결하는 과정에서 개발자와 퍼블리셔사의 소통이 엄청 많은 작업을 서로의 의견 교환과 원활한 소통으로 이 프로젝트에 문제가 생기지 않고 제작할 수 있었습니다. 또한 이 프로젝트에서는 웹 표준과 웹 접근성을 상당히 중요시 해 이미지의 alt, button에 title, aria-label과 같은 대체 텍스트를 사용하며 어떠한 사용자도 이 컨텐츠를 사용하면서 제약사항이 없도록 제작했습니다.`,
+    desc: `천재교육에서 진행하는 AIDT 고등학교 영어에 선임 퍼블리셔로 제작에 참여했습니다.<br>신입 혹은 다른 팀원 분들이 쉽게 제작할 수 있도록 하기위한 프로토 제작과 다른 여러 페이지 제작을 도맡아 했으며 디자인팀, 개발팀, 그리고 기획사와도 소통하며 제작을 하였습니다.<br>
+    프로젝트에서 처음 시도하는 api를 연결하는 과정에서 개발자와 퍼블리셔사의 소통이 엄청 많은 작업을 서로의 의견 교환과 원활한 소통으로 이 프로젝트에 문제가 생기지 않게 제작할 수 있었습니다.<br>
+    또한 이 프로젝트에서는 웹 표준과 웹 접근성을 상당히 중요시 해 이미지의 alt, button에 title, aria-label과 같은 대체 텍스트를 사용하며 어떠한 사용자도 이 컨텐츠를 사용하면서 제약사항이 없도록 제작했습니다.`,
     imgSrc1: "code1",
     imgSrc2: "code1"
   },
   {
     title: "고등학교 영어 전자저작물",
     skill: ["html", "css"],
-    desc: `천재교육에서 진행하는 고등학교 영어 전자저작물에 선임 퍼블리셔로 제작에 참여했습니다. 디자인팀, 개발팀, 기획사와 소통하며 제작을 하였습니다. 프로토 제작을 하며 최대한 코드가 지저분 하지않고 재사용이 가능한 css를 만들기위해 고민을 많이 했던 프로젝트 입니다. 또한 여러 사람들의 코드가 모여 불필요한 css가 너무 많아 가독성을 높이고 재사용이 가능한 css로 정리하였습니다.`,
+    desc: `천재교육에서 진행하는 고등학교 영어 전자저작물에 선임 퍼블리셔로 제작에 참여했습니다.<br>
+    디자인팀, 개발팀, 기획사와 소통하며 제작을 하였습니다. 프로토 제작을 하며 최대한 코드가 지저분 하지않고 재사용이 가능한 css를 만들기위해 고민을 많이 했던 프로젝트 입니다. 또한 여러 사람들의 코드가 모여 불필요한 css가 너무 많아 가독성을 높이고 재사용이 가능한 css로 정리하였습니다.`,
     imgSrc1: "code1",
     imgSrc2: "code1"
   }
