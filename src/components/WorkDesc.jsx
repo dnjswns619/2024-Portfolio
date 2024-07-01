ad8c72a8ecb6d485b5244dd5c31e24b33d01fd49
dnjswns619 <dnjswns619@naver.com>
2024-07-01 15:00:19 +0900

    feat : Add workDesc scroll animation

--- src/components/WorkDesc.jsx
+++ src/components/WorkDesc.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { workContent } from "../constants";
 import ProjectDesc from "./ProjectDesc";
-import codeImg1 from "../assets/images/code1.png"
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 const WorkDesc = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const workItemAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#workDesc",
+        start: "0% 80%",
+        end: "100% 100%",
+        scrub: 0
+      }
+    })
+    workItemAnimation
+    .fromTo(".workDesc__item1 .codeImg__wrap--1", {rotate: 10}, {rotate: -10, duration: 0.3}, "a")
+    .fromTo(".workDesc__item1 .codeImg__wrap--2", {rotate: 10}, {rotate: 1, duration: 0.3}, "a")
+    .fromTo(".workDesc__item2 .codeImg__wrap--1", {rotate: 10}, {rotate: -10, duration: 0.3}, "b")
+    .fromTo(".workDesc__item2 .codeImg__wrap--2", {rotate: 10}, {rotate: 1, duration: 0.3}, "b")
+    .fromTo(".workDesc__item3 .codeImg__wrap--1", {rotate: 10}, {rotate: -10, duration: 0.3}, "c")
+    .fromTo(".workDesc__item3 .codeImg__wrap--2", {rotate: 10}, {rotate: 1, duration: 0.3}, "c")
+  }, []);
+
   return (
     <section id="workDesc" className="workDesc">
       <div className="workDesc__wrap">
         {
-          workContent.map((item) => {
+          workContent.map((item, idx) => {
             return (
-              <div className="workDesc__item">
+              <div className={`workDesc__item workDesc__item${idx + 1}`}>
                 <div className="workDesc__item--img">
-                  <img src={require('../assets/images/' + item.imgSrc1 + '.png')} alt="" />
-                  <img src={require('../assets/images/' + item.imgSrc2 + '.png')} alt="" />
+                  <div className="codeImg__wrap codeImg__wrap--1">
+                    <img className="codeImg codeImg1" src={require('../assets/images/' + item.imgSrc1 + '.png')} alt="" />
+                  </div>
+                  <div className="codeImg__wrap codeImg__wrap--2">
+                    <img className="codeImg codeImg2" src={require('../assets/images/' + item.imgSrc2 + '.png')} alt="" />
+                  </div>
                 </div>
                 <ProjectDesc data={item} />
               </div>
