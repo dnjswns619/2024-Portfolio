bd573f05d6f295b5617d3ac39bd6744899c1d643
dnjswns619 <dnjswns619@naver.com>
2024-07-02 22:55:31 +0900

    refactor : Modify props received from projectDesc component

--- src/components/ProjectDesc.jsx
+++ src/components/ProjectDesc.jsx
@@ -1,20 +1,21 @@
 const ProjectDesc = ({data}) => {
+  const {title, skill, desc, gitLink} = {...data}
   return (
     <div className="projectDesc">
-      <div className="projectDesc__title">{data.title}</div>
+      <div className="projectDesc__title">{title}</div>
         <div className="projectDesc__skill">
           <div className="projectDesc__skill--tit">Language :</div>
-          <div className="projectDesc__skill--text">{data.skill.join(", ")}</div>
+          <div className="projectDesc__skill--text">{skill.join(", ")}</div>
         </div>
         <div className="projectDesc__desc">
           <div className="projectDesc__desc--tit">Description :</div>
-          <p className="projectDesc__desc--text" dangerouslySetInnerHTML={{__html: data.desc}}></p>
+          <p className="projectDesc__desc--text" dangerouslySetInnerHTML={{__html: desc}}></p>
         </div>
         {
-          data.gitLink ? 
+          gitLink ? 
           <div className="projectDesc__git-link">
             <div className="projectDesc__git-link--tit">GitHub: </div>
-            <a href={data.gitLink} className="projectDesc__gitLink">{data.gitLink}</a>
+            <a href={gitLink} className="projectDesc__gitLink">{gitLink}</a>
           </div> : null
         }
     </div>
