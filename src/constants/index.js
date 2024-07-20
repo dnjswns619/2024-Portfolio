cfbf282596f8fc3079ed4e36a937fae6aa0645db
dnjswns619 <dnjswns619@naver.com>
2024-07-20 22:08:18 +0900

    style : Modoify the number of work projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -107,7 +107,8 @@ export const portContent = [
     모든 컨텐츠를 tab키로 이동할수 있도록 했으며 a태그의 title, img태그의 alt 작성으로
     눈으로 보지 못하는 사용자도 컨텐츠를 이해할 수 있도록 제작했습니다`,
     gitLink: `https://github.com/dnjswns619/MCST`,
-    imgSrc: "intro_circle0"
+    imgSrc: "MCST.png",
+    pageLink: "https://dnjswns619.github.io/MCST/"
   },
   {
     id: 2,
@@ -118,7 +119,8 @@ export const portContent = [
     고해상도의 이미지가 깨지지 않고 부드럽게 스크롤 하며 텍스트들이
     스크롤 정도에 따라 나오거나 들어가도록 제작하였습니다.`,
     gitLink: `https://github.com/dnjswns619/Apple_AirPods`,
-    imgSrc: "intro_circle0"
+    imgSrc: "intro_circle0.jpg",
+    pageLink: "https://dnjswns619.github.io/MCST/"
   },
   {
     id: 3,
@@ -128,7 +130,8 @@ export const portContent = [
     observer API를 같이 사용해 사용자가 특정 섹션에 들어왔을때 header 메뉴가 변화하도록 제작하였으며,
     반응형에서도 컨텐츠들의 위치들이 무너지지 않고 서로 다른 애니메이션 효과등을 사용하도록 제작하였습니다.`,
     gitLink: `https://github.com/dnjswns619/enterpriseblockchain`,
-    imgSrc: "intro_circle0"
+    imgSrc: "enterpriseblockchain.png",
+    pageLink: "https://dnjswns619.github.io/enterpriseblockchain/"
   },
   {
     id: 4,
@@ -138,85 +141,44 @@ export const portContent = [
     슬라이더는 swiper slide를 사용하였고 컴포넌트를 나누고 여러 데이터들을 전달하는 과정에서 react를 이해하며 사용할 수 있었습니다.
     `,
     gitLink: `https://github.com/dnjswns619/Kream`,
-    imgSrc: "intro_circle0"
+    imgSrc: "kream.png",
+    pageLink: "https://dnjswns619.github.io/MCST/"
   }
 ]
 
 export const workImg = [
   {
-    imgSrc: "0 (1)"
+    imgSrc: "poster(1)"
   },
   {
-    imgSrc: "0 (2)"
+    imgSrc: "poster(2)"
   },
   {
-    imgSrc: "0 (3)"
+    imgSrc: "poster(3)"
   },
   {
-    imgSrc: "0 (4)"
+    imgSrc: "poster(4)"
   },
   {
-    imgSrc: "0 (5)"
+    imgSrc: "poster(5)"
   },
   {
-    imgSrc: "0 (6)"
+    imgSrc: "poster(6)"
   },
   {
-    imgSrc: "0 (7)"
+    imgSrc: "poster(7)"
   },
   {
-    imgSrc: "0 (8)"
+    imgSrc: "poster(8)"
   },
   {
-    imgSrc: "0 (9)"
+    imgSrc: "poster(9)"
   },
   {
-    imgSrc: "0 (10)"
+    imgSrc: "poster(10)"
   },
   {
-    imgSrc: "0 (11)"
-  },
-  {
-    imgSrc: "0 (12)"
-  },
-  {
-    imgSrc: "0 (13)"
-  },
-  {
-    imgSrc: "0 (14)"
-  },
-  {
-    imgSrc: "0 (15)"
-  },
-  {
-    imgSrc: "0 (16)"
-  },
-  {
-    imgSrc: "0 (17)"
-  },
-  {
-    imgSrc: "0 (18)"
-  },
-  {
-    imgSrc: "0 (19)"
-  },
-  {
-    imgSrc: "0 (20)"
-  },
-  {
-    imgSrc: "0 (21)"
-  },
-  {
-    imgSrc: "0 (22)"
-  },
-  {
-    imgSrc: "0 (23)"
-  },
-  {
-    imgSrc: "0 (24)"
-  },
-  {
-    imgSrc: "0 (25)"
+    imgSrc: "poster(11)"
   }
 ]
 
@@ -226,28 +188,19 @@ export const workContent = [
     title: "초등학교 3,4학년 과학",
     skill: ["html", "css", "javascript"],
     desc: `천재교육에서 진행하는 초등학교 3,4 학년 전자저작물 제작에 참여했습니다.<br>
-    입사후 처음으로 진행한 프로젝트이며 디자인팀과 소통하고 개발이 필요할 때에는 개발 팀과 소통하며 업무를 진행해 나아갔고 200페이지가 넘는 쪽수를 개발 했습니다.<br>
+    입사 후 처음으로 진행한 프로젝트이며 디자인팀과 소통하고 개발이 필요할 때에는 개발 팀과 소통하며 업무를 진행해 나아갔고 680 페이지 가량의 쪽수를 제작 했습니다.<br>
     html과 css를 이용해 작업하였으며 개발팀에서 제작해 놓았던 템플릿과 개발된 코드등을 수정해 다른 페이지에도 적용해가며 촉박한 기한에도 불구하고 완성시켜 제출할 수 있엇습니다.`,
-    imgSrc1: "code1",
-    imgSrc2: "code1"
+    imgSrc1: "sci_code2",
+    imgSrc2: "sci_code1"
   },
   {
     id: 2,
     title: "AIDT 고등학교 영어",
-    skill: ["html", "css"],
-    desc: `천재교육에서 진행하는 AIDT 고등학교 영어에 선임 퍼블리셔로 제작에 참여했습니다.<br>신입 혹은 다른 팀원 분들이 쉽게 제작할 수 있도록 하기위한 프로토 제작과 다른 여러 페이지 제작을 도맡아 했으며 디자인팀, 개발팀, 그리고 기획사와도 소통하며 제작을 하였습니다.<br>
-    프로젝트에서 처음 시도하는 api를 연결하는 과정에서 개발자와 퍼블리셔사의 소통이 엄청 많은 작업을 서로의 의견 교환과 원활한 소통으로 이 프로젝트에 문제가 생기지 않게 제작할 수 있었습니다.<br>
-    또한 이 프로젝트에서는 웹 표준과 웹 접근성을 상당히 중요시 해 이미지의 alt, button에 title, aria-label과 같은 대체 텍스트를 사용하며 어떠한 사용자도 이 컨텐츠를 사용하면서 제약사항이 없도록 제작했습니다.`,
-    imgSrc1: "code1",
-    imgSrc2: "code1"
+    skill: ["html", "css", "javascript"],
+    desc: `천재교육에서 진행하는 AIDT 고등학교 영어에 선임 퍼블리셔로 제작에 참여했습니다.<br>프로토를 제작하며 공통으로 쓰이는 css를 만들었고 개발팀 템플릿을 활용한 페이지 개발을 이끌어 나갔습니다.<br>
+    회사에서 처음 시도하는 API를 연결 과정에서 개발자와 퍼블리셔 사이의 소통이 많은 작업을 서로의 의견 교환과 원활한 소통으로 프로젝트를 문제 없이 제작할 수 있었습니다.<br>
+    또한 이 프로젝트에서는 웹 표준과 웹 접근성을 상당히 중요시 해 이미지의 alt, button에 aria-label 혹은 IR기법 등을 사용하며 어떠한 사용자도 이 컨텐츠를 사용하면서 제약사항이 없도록 제작했습니다.`,
+    imgSrc1: "aidt_code2",
+    imgSrc2: "aidt_code1"
   },
-  {
-    id: 3,
-    title: "고등학교 영어 전자저작물",
-    skill: ["html", "css"],
-    desc: `천재교육에서 진행하는 고등학교 영어 전자저작물에 선임 퍼블리셔로 제작에 참여했습니다.<br>
-    디자인팀, 개발팀, 기획사와 소통하며 제작을 하였습니다. 프로토 제작을 하며 최대한 코드가 지저분 하지않고 재사용이 가능한 css를 만들기위해 고민을 많이 했던 프로젝트 입니다. 또한 여러 사람들의 코드가 모여 불필요한 css가 너무 많아 가독성을 높이고 재사용이 가능한 css로 정리하였습니다.`,
-    imgSrc1: "code1",
-    imgSrc2: "code1"
-  }
 ]
